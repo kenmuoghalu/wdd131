f66a3ada63ab1f5397f01ab610a83e04656ddec9
kenmuoghalu <kenmuoghalu944@gmail.com>
2025-09-13 06:03:16 +0100

    changes

--- styles/scripts/getdates.js
+++ styles/scripts/getdates.js
@@ -1,11 +1,7 @@
 const year = document.querySelector("#currentyear");
 const modification = document.querySelector("#lastModified");
 const today = new Date();
-const modificationDate = new Date(document.lastModified);
-
-// Format the modification date for better readability
-const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-const formattedModDate = modificationDate.toLocaleDateString('en-US', options);
+const modificationDate = document.lastModified;
 
 year.innerHTML = `<span id="currentyear">&copy ${today.getFullYear()}</span>`;
-modification.innerHTML = `<p id="lastModified">Last Modified: ${formattedModDate}</p>`;
+modification.innerHTML = `<p id="lastModified">Last Modified: ${modificationDate}</p>`;
